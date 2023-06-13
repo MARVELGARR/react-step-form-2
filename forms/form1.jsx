c698f20bbdd70913945976ea5be10849490d7631
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-06-13 09:27:41 +0100

    added new features

--- forms/form1.jsx
+++ forms/form1.jsx
@@ -2,25 +2,28 @@ import React from 'react'
 
 function Form1() {
   return (
-    <form className="">
-      <h1 className=" text-md">Personal info</h1>
-      <p className="">Please provide your name, email address, and phone number.</p>
-      <div>
-        <div>
-          <label className="" for="name">Name</label>
-          <input type="text" id='name' className=""/ >
+    <form className=" flex flex-col gap-5">
+      <div className="">
+        <h1 className="">Personal info</h1>
+        <p className="">Please provide your name, email address, and phone number.</p>
+      </div>
+      
+      <div className="">
+        <fieldset>
+          <label className="">Name</label>
+          <input type="text" id='name' className="" />
           <div className=''></div>
-        </div>
-        <div>
-          <label className="" for="email">Email Address</label>
-          <input type="email" id="email" className=""/ >
+        </fieldset>
+        <fieldset>
+          <label className="">Email Address</label>
+          <input type="email" id="email" className="" />
           <div className=''></div>
-        </div>
-        <div>
-          <label className="" for="number">Phone Number</label>
-          <input type="number" id="number" className=""/ >
+        </fieldset>
+        <fieldset>
+          <label className="">Phone Number</label>
+          <input type="number" id="number" className="" />
           <div className=''></div>
-        </div>
+        </fieldset>
       </div>
     </form>
   )
