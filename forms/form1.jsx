8f1a52ce8faaf949afc7ef23664b7366a2d93d2f
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-06-14 22:03:33 +0100

    working on the form

--- forms/form1.jsx
+++ forms/form1.jsx
@@ -3,25 +3,25 @@ import "../src/index.css"
 
 function Form1() {
   return (
-    <form className="w-full  flex flex-col">
+    <form className="w-full px-7 flex flex-col">
       <div className=" ">
-        <h1 className="">Personal info</h1>
-        <p className=" ">Please provide your name, email address, and phone number.</p>
+        <h1 className="text-3xl font-bold text-cyan-800">Personal info</h1>
+        <p className=" text-xl text-gray-500">Please provide your name, email address, and phone number.</p>
       </div>
 
       <div className="">
-        <fieldset>
-          <label className="" htmlFor='name'>Name</label>
-          <input type="text" id='name' className="" />
+        <fieldset className='flex flex-col'>
+          <label className=" font-bold text-lg" htmlFor='name'>Name</label>
+          <input type="text" id='name' className=" border-5" />
           <div className=''></div>
         </fieldset>
-        <fieldset>
-          <label className="">Email Address</label>
+        <fieldset className='flex flex-col'>
+          <label className="font-bold text-lg" htmlFor='email'>Email Address</label>
           <input type="email" id="email" className="" />
           <div className=''></div>
         </fieldset>
-        <fieldset>
-          <label className="">Phone Number</label>
+        <fieldset className='flex flex-col'>
+          <label className="font-bold text-lg" htmlFor='number'>Phone Number</label>
           <input type="number" id="number" className="" />
           <div className=''></div>
         </fieldset>
