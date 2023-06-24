0bbbb78d16bdc7e28339e17974c9fef2343c0031
MARVELGARR <75810831+MARVELGARR@users.noreply.github.com>
2023-06-24 22:01:17 +0100

    creating a toggle swich for my plans

--- forms/form2.jsx
+++ forms/form2.jsx
@@ -3,20 +3,21 @@ import { MyContext } from '../state managemant/MyContex'
 
 function Form2() {
   const { handleBack, handleNext, handPeriodChange, periodicy, setPeriodicy} = useContext(MyContext);
-  const [plan, setPlan] = useState([]);
-  const [price, setPrice] = useState([]);
+  const [plan, setPlan] = useState('');
+  const [price, setPrice] = useState('');
+  const [selectedPlan, setSelectedPlan] = useState([]);
 
   const handleSelectRadio = (index) =>{
     if(index === 0){
-      setPlan([...plan, "arcade"]);
+      setPlan("arcade");
       console.log(plan); 
     }
     if(index === 1){
-      setPlan([...plan, "advance"]);
+      setPlan("advance");
       console.log(plan); 
     }
     if(index === 2){
-      setPlan([...plan, "pro"]);
+      setPlan("pro");
       console.log(plan); 
     }
   }
@@ -30,34 +31,41 @@ function Form2() {
         </div>
       </div>
       <div className="flex flex-col gap-3">
-        <fieldset className="flex border-2 rounded-lg p-3 gap-5 items-center">
+        <fieldset className="flex relative border-2 rounded-lg p-3 gap-5 items-center">
           <img className="" src="/images/icon-arcade.svg"/>
-          <input onClick={()=>handleSelectRadio(0)} type='radio' name='radio' value='Arcade'/>
+          <input onClick={()=>handleSelectRadio(0)} className='absolute opacity-0 left-0 right-0 top-0 bottom-0' type='radio' name='radio' value='Arcade'/>
           <div className="">
             <strong className='text-cyan-900 bold'>Arcade</strong>
-            <div className="">$9/mo</div>
+            <div className="">{ periodicy ? `$9/mo` : "$90/yr"}</div>
           </div>
         </fieldset>
-        <fieldset className=" flex border-2 gap-5 p-3 rounded-lg items-center">
+        <fieldset className=" flex relative border-2 gap-5 p-3 rounded-lg items-center">
           <img className="" src="/images/icon-advanced.svg"/>
-          <input onClick={()=>handleSelectRadio(1)} type='radio' name='radio' value='Advance'/>
+          <input onClick={()=>handleSelectRadio(1)} className='absolute opacity-0 left-0 right-0 top-0 bottom-0' type='radio' name='radio' value='Advance'/>
           <div className="">
             <strong className='text-cyan-900 bold'>Advanced</strong>
-            <div className="">$12/mo</div>
+            <div className="">{ periodicy ? `$12/mo`: "$120/yr"}</div>
           </div>
         </fieldset>
-        <fieldset className="flex border-2 gap-5 p-3 rounded-lg items-center">
+        <fieldset className="flex relative border-2 gap-5 p-3 rounded-lg items-center">
           <img className="" src="/images/icon-pro.svg"/>
-          <input onClick={()=>handleSelectRadio(2)} type='radio' name='radio' value='Pro'/>
+          <input onClick={()=>handleSelectRadio(2)} className="absolute opacity-0 left-0 right-0 top-0 bottom-0" type='radio' name='radio' value='Pro'/>
           <div className="">
             <strong className='text-cyan-900 bold'>Pro</strong>
-            <div className="">$15/mo</div>
+            <div className="">{ periodicy ? `$15/mo` : "$150/yr"}</div>
           </div>
         </fieldset>
       </div>
       <div className="flex">
         <strong className="">monthly</strong>
-        <div className=""></div>
+        <div className="">
+        <label class="relative inline-flex items-center mb-5 cursor-pointer">
+          <input onClick={handPeriodChange} type="checkbox" value="" class="sr-only peer"/>
+          <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Small toggle</span>
+        </label>
+
+        </div>
         <div className="">yearly</div>
       </div>
       <div className="w-full flex justify-between ">
